1b5e3764823cef885dfee8bb650de3ccac2a7b70
Alexandre Richet <alexandre.richet@epitech.eu>
2017-12-10 18:29:58 +0100

    Add interface teminal for choice device and list this
    Add interface content list device to connect and propose select item in demonstration project "hfdial" i'm listing modification
    
    1: Update Nuget package 
    2: Add Nuget package "32feet.NET"
    3: Add/Change code in Program.cs on main function
    4: Add Device Class for save list item

--- hfdial/Program.cs
+++ hfdial/Program.cs
@@ -17,8 +17,26 @@ namespace hfdial
             log4net.Config.BasicConfigurator.Configure();
             try
             {
+                /*
+                 * List and choice selected Item
+                 */
+                List<Device> devices = new List<Device>();
+                InTheHand.Net.Sockets.BluetoothClient bc = new InTheHand.Net.Sockets.BluetoothClient();
+                InTheHand.Net.Sockets.BluetoothDeviceInfo[] array = bc.DiscoverDevices();
+                int count = array.Length;
+                for (int i = 0; i < count; i++)
+                {
+                    Device device = new Device(array[i]);
+                    devices.Add(device);
+                    Console.Write("" + (i + 1) + ": " + device.ToString() + "\n");
+                }
+                Console.Write("Please enter the device numbers\n");
+                var choiceDevice = Convert.ToInt32(Console.ReadLine());
+
+                /* ***************************** */
+
                 var d = new SimpleDialer();
-                d.Dial(args[0], args[1]).Wait();
+                d.Dial("1", devices[choiceDevice - 1].DeviceName).Wait(); /* Apply parameter */
                 Thread.Sleep(TimeSpan.FromSeconds(30));
                 return 0;
             }
@@ -26,6 +44,35 @@ namespace hfdial
             {
                 log.Error(e);
                 return -1;
+            }
+        }
+
+        class Device
+        {
+            public string DeviceName { get; set; }
+            public bool Authenticated { get; set; }
+            public bool Connected { get; set; }
+            public ushort Nap { get; set; }
+            public uint Sap { get; set; }
+            public DateTime LastSeen { get; set; }
+            public DateTime LastUsed { get; set; }
+            public bool Remembered { get; set; }
+
+            public Device(InTheHand.Net.Sockets.BluetoothDeviceInfo device_info)
+            {
+                this.Authenticated = device_info.Authenticated;
+                this.Connected = device_info.Connected;
+                this.DeviceName = device_info.DeviceName;
+                this.LastSeen = device_info.LastSeen;
+                this.LastUsed = device_info.LastUsed;
+                this.Nap = device_info.DeviceAddress.Nap;
+                this.Sap = device_info.DeviceAddress.Sap;
+                this.Remembered = device_info.Remembered;
+            }
+
+            public override string ToString()
+            {
+                return this.DeviceName;
             }
         }
     }
